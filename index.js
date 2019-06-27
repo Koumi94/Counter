116b3fd2fc7af6059384c2a5a0392c2541e04c68
Muaz Yediyüzkırkiki <39378729+muaz742@users.noreply.github.com>
2019-06-27 16:29:13 +0300

    added countdown block

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-$( "#test" ).on( "click", function( event ) {
-    alert("index.js is run 👌");
-  });
+$("#buttonTest").on("click", function (event) {
+  alert("index.js is run 👌");
+});
+
+$('#buttonStart').on("click", function (event) {
+  countdown();
+});
+
+var second = 10;
+function countdown() {
+  if (second != 0) {
+    second -= 1;
+    document.getElementById('viewSecond').innerHTML = second;
+  }
+  else {
+    alert('countdown complete');
+    document.getElementById('viewSecond').innerHTML = 10;
+    second = 10;
+    return;
+  }
+  setTimeout("countdown()", 1000);
+}
